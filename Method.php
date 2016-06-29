8a2e931a20b8623c53d9243a50f80beda541d0a3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-06-29 23:50:30 +0300

    no message

---
+++ Method.php
@@ -0,0 +1,76 @@
+<?php
+namespace Dfe\AllPay;
+use Dfe\AllPay\Settings as S;
+use Exception as E;
+use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException as LE;
+use Magento\Payment\Model\Info as I;
+use Magento\Payment\Model\InfoInterface as II;
+use Magento\Sales\Model\Order as O;
+use Magento\Sales\Model\Order\Address as OrderAddress;
+use Magento\Sales\Model\Order\Creditmemo;
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Sales\Model\Order\Payment as OP;
+use Magento\Sales\Model\Order\Payment\Transaction;
+class Method extends \Df\Payment\Method {
+	/**
+	 * 2016-06-29
+	 * @override
+	 * @see \Df\Payment\Method::capture()
+	 *
+	 * $amount содержит значение в учётной валюте системы.
+	 * https://github.com/magento/magento2/blob/6ce74b2/app/code/Magento/Sales/Model/Order/Payment/Operations/CaptureOperation.php#L37-L37
+	 * https://github.com/magento/magento2/blob/6ce74b2/app/code/Magento/Sales/Model/Order/Payment/Operations/CaptureOperation.php#L76-L82
+	 *
+	 * @param II|I|OP $payment
+	 * @param float $amount
+	 * @return $this
+	 * @throws E|LE
+	 */
+	public function capture(II $payment, $amount) {
+		return $this;
+	}
+
+	/**
+	 * @override
+	 * @see \Df\Payment\Method::getConfigPaymentAction()
+	 * @return string
+	 *
+	 * 2016-05-07
+	 * Сюда мы попадаем только из метода @used-by \Magento\Sales\Model\Order\Payment::place()
+	 * причём там наш метод вызывается сразу из двух мест и по-разному.
+	 *
+	 * 2016-06-29
+	 * Умышленно возвращаем null.
+	 * @used-by \Magento\Sales\Model\Order\Payment::place()
+	 * https://github.com/magento/magento2/blob/ffea3cd/app/code/Magento/Sales/Model/Order/Payment.php#L334-L355
+	 */
+	public function getConfigPaymentAction() {return null;}
+
+	/**
+	 * 2016-06-29
+	 * @override
+	 * @see \Df\Payment\Method::getInfoBlockType()
+	 * @return string
+	 */
+	public function getInfoBlockType() {return \Magento\Payment\Block\Info\Cc::class;}
+
+	/**
+	 * 2016-06-29
+	 * @override
+	 * @see \Df\Payment\Method::setStore()
+	 * @param int $storeId
+	 * @return void
+	 */
+	public function setStore($storeId) {
+		parent::setStore($storeId);
+		S::s()->setScope($storeId);
+	}
+
+	/**
+	 * 2016-06-29
+	 * @used-by Dfe/AllPay/etc/frontend/di.xml
+	 * @used-by \Dfe\AllPay\ConfigProvider::getConfig()
+	 */
+	const CODE = 'dfe_all_pay';
+}
---
+++ Settings.php
@@ -0,0 +1,127 @@
+<?php
+namespace Dfe\AllPay;
+use Magento\Framework\App\ScopeInterface as S;
+class Settings extends \Df\Core\Settings {
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Description»
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	public function description($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Enable?»
+	 * @param null|string|int|S $s [optional]
+	 * @return bool
+	 */
+	public function enable($s = null) {return $this->b(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	public function hashIV($s = null) {
+		return $this->test($s) ? $this->testHashIV($s) : $this->liveHashIV($s);
+	}
+
+	/**
+	 * 2016-06-29
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	public function hashKey($s = null) {
+		return $this->test($s) ? $this->testHashKey($s) : $this->liveHashKey($s);
+	}
+
+	/**
+	 * 2016-06-29
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	public function merchantID($s = null) {
+		return $this->test($s) ? $this->testMerchantID($s) : $this->liveMerchantID($s);
+	}
+
+	/**
+	 * 2016-06-29
+	 * @param null|string|int|S $s [optional]
+	 * @return string|null
+	 */
+	public function platformID($s = null) {return $this->test($s) ? null : $this->livePlatformID($s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Test Mode?»
+	 * @param null|string|int|S $s [optional]
+	 * @return bool
+	 */
+	public function test($s = null) {return $this->b(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Live ALL IN ONE 介接 HashIV»
+	 * Note that we encrypt the live keys, but do not encrypt the test keys.
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function liveHashIV($s = null) {return $this->p(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Live ALL IN ONE 介接 HashKey»
+	 * Note that we encrypt the live keys, but do not encrypt the test keys.
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function liveHashKey($s = null) {return $this->p(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Live Merchant ID (商店代號)»
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function liveMerchantID($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Live Platform ID»
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function livePlatformID($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Test ALL IN ONE 介接 HashIV»
+	 * Note that we encrypt the live keys, but do not encrypt the test keys.
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function testHashIV($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Test ALL IN ONE 介接 HashKey»
+	 * Note that we encrypt the live keys, but do not encrypt the test keys.
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function testHashKey($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/**
+	 * 2016-06-29
+	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay» → «Test Merchant ID (商店代號)»
+	 * @param null|string|int|S $s [optional]
+	 * @return string
+	 */
+	private function testMerchantID($s = null) {return $this->v(__FUNCTION__, $s);}
+
+	/** @return self */
+	public static function s() {static $r; return $r ? $r : $r = df_o(__CLASS__);}
+}
+
+
