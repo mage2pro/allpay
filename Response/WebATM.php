37d861007180248854430bf7f83be607d9d52a75
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-07-26 12:27:59 +0300

    0.9.3

---
+++ Response/WebATM.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dfe\AllPay\Response;
+class WebATM extends \Dfe\AllPay\Response {
+	/**
+	 * 2016-07-26
+	 * @override
+	 * @see \Df\Payment\R\Response::needCapture()
+	 * @used-by \Df\Payment\R\Response::handle()
+	 * @return bool
+	 */
+	protected function needCapture() {return true;}
+}
+
