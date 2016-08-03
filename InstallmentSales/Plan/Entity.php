d9e121dd7fd3307a5dec8a067f20ec29aad7bfd5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-03 16:07:40 +0300

    0.9.8

--- InstallmentSales/Plan/Entity.php
+++ InstallmentSales/Plan/Entity.php
@@ -1,12 +1,23 @@
 <?php
 namespace Dfe\AllPay\InstallmentSales\Plan;
-class Entity extends \Df\Config\O {
+use Df\Core\Exception as DFE;
+class Entity extends \Df\Config\ArrayItem {
 	/**
 	 * 2016-07-31
 	 * @override
-	 * @see \Df\Config\O::getId()
+	 * @see \Df\Config\ArrayItem::getId()
 	 * @used-by \Df\Config\A::get()
 	 * @return int
 	 */
-	public function getId() {return $this['count']['value'];}
+	public function getId() {return $this['count'];}
+
+	/**
+	 * 2016-08-02
+	 * @override
+	 * @see \Df\Config\O::validate()
+	 * @used-by \Df\Config\Backend\Serialized::validate()
+	 * @return void
+	 * @throws DFE
+	 */
+	public function validate() {df_assert(!is_array($this['count']));}
 }
--- InstallmentSales/Plan/FormElement.php
+++ InstallmentSales/Plan/FormElement.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dfe\AllPay\InstallmentSales\Plan;
 use Df\Framework\Form\Element\Fieldset;
+use Df\Framework\Form\Element\Number;
 /**
  * 2015-12-27
  * Этот класс не является одиночкой:
@@ -19,8 +20,8 @@ class FormElement extends Fieldset {
 		// 2016-07-30
 		// Этот стиль будет применён к элементу <fieldset>.
 		$this->addClass('dfe-allpay-installment-plan');
-		$this->quantity('count', 'Number of Monthly Payments');
-		$this->percent('rate', 'Interest Rate', 0);
+		$this->number('count', 'Number of Monthly Payments');
+		$this->percent('rate', 'Interest Rate');
 		$this->money('fee', 'Fixed Monthly Fee');
 		df_fe_init($this, __CLASS__, [], [], 'plan');
 	}
