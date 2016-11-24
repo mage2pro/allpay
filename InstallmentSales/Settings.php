1776ba4f93c5625c6a7d931bb38cca16486b6296
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-24 03:38:43 +0300

    1.1.11

--- InstallmentSales/Settings.php
+++ InstallmentSales/Settings.php
@@ -3,7 +3,7 @@ namespace Dfe\AllPay\InstallmentSales;
 use Df\Config\A;
 use Dfe\AllPay\InstallmentSales\Plan\Entity as Plan;
 /** @method static Settings s() */
-final class Settings extends \Df\Core\Settings {
+final class Settings extends \Df\Config\Settings {
 	/**
 	 * 2016-07-31
 	 * «Mage2.PRO» → «Payment» → «歐付寶 allPay»→ «Installment Sales» → «Plans»
@@ -19,9 +19,9 @@ final class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-07-31
 	 * @override
-	 * @see \Df\Core\Settings::prefix()
-	 * @used-by \Df\Core\Settings::v()
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
 	 * @return string
 	 */
-	protected function prefix() {return 'df_payment/all_pay/installment_sales/';}
+	protected function prefix() {return 'df_payment/all_pay/installment_sales';}
 }
