21b9c1b40817a41c213ee3a272665688ba5981b5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-21 10:25:21 +0300

    no message

--- Controller/CustomerReturn/Index.php
+++ Controller/CustomerReturn/Index.php
@@ -2,12 +2,12 @@
 namespace Dfe\AllPay\Controller\CustomerReturn;
 // 2017-02-14
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Index extends \Df\Payment\Action\CustomerReturn {
+class Index extends \Df\Payment\CustomerReturn {
 	/**
 	 * 2016-08-27
 	 * @override
-	 * @see \Df\Payment\Action\CustomerReturn::message()
-	 * @used-by \Df\Payment\Action\CustomerReturn::execute()
+	 * @see \Df\Payment\CustomerReturn::message()
+	 * @used-by \Df\Payment\CustomerReturn::execute()
 	 * @return string
 	 */
 	protected function message() {return $this->transP('RtnMsg');}
