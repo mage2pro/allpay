b443cbc0c5dcd838f58f77c0013c91b4b30be0b3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-22 15:48:26 +0300

    no message

--- W/Nav/Offline.php
+++ W/Nav/Offline.php
@@ -9,9 +9,9 @@ final class Offline extends \Df\PaypalClone\W\Nav {
 	 * 2017-01-04
 	 * Своим поведением этот метод напоминает мне @see \Df\StripeClone\Method::e2i()
 	 * @override
-	 * @see \Df\Payment\W\Handler::id()
-	 * @used-by \Df\Payment\W\Handler::ii()
+	 * @see \Df\Payment\W\Handler::type()
+	 * @used-by \Df\Payment\W\Handler::id()
 	 * @return string
 	 */
-	protected function id() {return parent::id() . ($this->e()->needCapture() ? '-capture' : '-info');}
+	protected function type() {return $this->e()->needCapture() ? 'capture' : 'info';}
 }
