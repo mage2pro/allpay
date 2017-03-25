c693bf3851db45cec55a69bbb60594f24c254fab
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-25 11:12:24 +0300

    no message

--- Block/Info/BankCard.php
+++ Block/Info/BankCard.php
@@ -19,11 +19,12 @@ class BankCard extends \Dfe\AllPay\Block\Info {
 		/** @var array(strig => string) $result */
 		$result = [];
 		$result['Card Number'] = df_ccc('******', $this->e('card6no', 'card4no'));
-		if (df_is_backend()) {
+		/** @var bool $b */
+		if ($b = df_is_backend()) {
 			$result['ECI'] = $this->eci();
 		}
 		$result['Authorization Code'] = $this->e('auth_code');
-		if (df_is_backend()) {$result += [
+		if ($b) {$result += [
 			'Authorization Time' => $this->e()->authTime()
 			// 2016-07-29
 			// [allPay] What does mean the «gwsr» response parameter?
