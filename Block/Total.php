34d988916cd70597365e88f8c3e13538cb70818c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-09 05:18:59 +0300

    no message

--- Block/Total.php
+++ Block/Total.php
@@ -6,20 +6,27 @@ use Dfe\AllPay\Total\Quote as TQuote;
 class Total extends \Df\Sales\Block\Order\Total {
 	/**
 	 * 2016-08-13
+	 * 2016-08-14
+	 * «grand_total» определено здесь: @see \Magento\Sales\Block\Order\Totals::_initTotals()
+	 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Block/Order/Totals.php#L150-L158
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
 	 * @see \Df\Sales\Block\Order\Total::initTotals()
 	 * @used-by \Magento\Sales\Block\Order\Totals::_beforeToHtml()
+	 *		protected function _beforeToHtml() {
+	 *			$this->_initTotals();
+	 *			foreach ($this->getLayout()->getChildBlocks($this->getNameInLayout()) as $child) {
+	 *				if (method_exists($child, 'initTotals') && is_callable([$child, 'initTotals'])) {
+	 *					$child->initTotals();
+	 *				}
+	 *			}
+	 *			return parent::_beforeToHtml();
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.2.0-RC1.8/app/code/Magento/Sales/Block/Order/Totals.php#L51-L65
 	 */
 	function initTotals() {
-		/** @var float|null $v */
-		/** @var float|null $b */
-		list($v, $b) = TQuote::iiGet($this->op());
+		list($v, $b) = TQuote::iiGet($this->op()); /** @var float|null $v */ /** @var float|null $b */
 		if ($v) {
-			/**
-			 * 2016-08-14
-			 * «grand_total» определено здесь: @see \Magento\Sales\Block\Order\Totals::_initTotals()
-			 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Block/Order/Totals.php#L150-L158
-			 */
 			$this->addBefore('dfe_allpay', 'Installment Fee', $v, $b, 'grand_total');
 		}
 	}
