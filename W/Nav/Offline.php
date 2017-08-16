3cc3b748709bb9f6828a2574c1463d9e6cd76dab
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-16 16:46:53 +0300

    no message

--- W/Nav/Offline.php
+++ W/Nav/Offline.php
@@ -1,12 +1,11 @@
 <?php
 namespace Dfe\AllPay\W\Nav;
-use Dfe\AllPay\W\Event;
 // 2017-03-15
-/** @method Event e() */
+/** @method \Dfe\AllPay\W\Event e() */
 final class Offline extends \Df\PaypalClone\W\Nav {
 	/**
 	 * 2016-07-20
-	 * 2017-01-04 Своим поведением этот метод напоминает мне @see \Df\StripeClone\Method::e2i()
+	 * 2017-01-04 It is implemented similar to @see \Df\StripeClone\Method::e2i()
 	 * @override
 	 * @see \Df\PaypalClone\W\Nav::type()
 	 * @used-by \Df\PaypalClone\W\Nav::id()
