c45337d8e3e60021a238a53bdc50ebe0b955dba7
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-16 18:08:57 +0000

    https://github.com/mage2pro/core/issues/174

--- TWD.php
+++ TWD.php
@@ -5,9 +5,7 @@ final class TWD {
 	 * 2016-08-15
 	 * @used-by \Dfe\AllPay\InstallmentSales\Plan\Entity::amount()
 	 * @used-by \Dfe\AllPay\Total\Quote::collect()
-	 * @param float $a
-	 * @param string $cCode
 	 * @return float|int
 	 */
-	static function round($a, $cCode) {return 'TWD' === $cCode ? round($a) : $a;}
+	static function round(float $a, string $cCode) {return 'TWD' === $cCode ? round($a) : $a;}
 }
