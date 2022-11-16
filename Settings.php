17dcba91fed90c3d611399d8c5eced4333e27f28
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-16 18:06:21 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings.php
+++ Settings.php
@@ -3,37 +3,34 @@
 namespace Dfe\AllPay;
 use Df\Config\Source\WaitPeriodType;
 use Df\Payment\Settings\Options as O;
+use Dfe\AllPay\InstallmentSales\Settings as SettingsI;
 use Dfe\AllPay\Source\Option as OptionSource;
 /** @method static Settings s() */
 final class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-07-01 «Description on a kiosk's screen»
-	 * @return string
 	 */
-	function descriptionOnKiosk() {return $this->v();}
+	function descriptionOnKiosk():string {return $this->v();}
 
 	/**
 	 * 2016-06-29 «ALL IN ONE 介接 HashIV»
 	 * Note that we encrypt the live keys, but do not encrypt the test keys.
-	 * @return string
 	 */
-	function hashIV() {return $this->testablePV();}
+	function hashIV():string {return $this->testablePV();}
 
 	/**
 	 * 2016-06-29 «LL IN ONE 介接 HashKey»
 	 * Note that we encrypt the live keys, but do not encrypt the test keys.
-	 * @return string
 	 */
-	function hashKey() {return $this->testablePV();}
+	function hashKey():string {return $this->testablePV();}
 
 	/**
 	 * 2016-07-31
 	 * @used-by \Dfe\AllPay\ConfigProvider::config()
 	 * @used-by \Dfe\AllPay\Method::plan()
 	 * @used-by \Dfe\AllPay\Total\Quote::collect()
-	 * @return InstallmentSales\Settings
 	 */
-	function installmentSales() {return $this->child(InstallmentSales\Settings::class);}
+	function installmentSales():SettingsI {return $this->child(SettingsI::class);}
 
 	/**
 	 * 2017-03-03
